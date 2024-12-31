9ed87f20d3cc8523dd8e34bad8ddfa4ae8348d48
hoeeeeeh <hoeeeeeh@naver.com>
2024-12-31 17:08:22 +0900

    feat: 첫 번째 이미지를 썸네일로

--- _plugins/post-image.rb
+++ _plugins/post-image.rb
@@ -2,12 +2,12 @@ module Jekyll
   module DefaultImage
     def default_image(image_path, default_path)
       source_path = File.join(Dir.pwd, image_path)
-
-      if File.exist?(source_path)
-        image_path
-      else
-        default_path
-      end
+      image_path
+#       if File.exist?(source_path)
+#         image_path
+#       else
+#         default_path
+#       end
     end
   end
 end
